9eeb4ff99252f9320aa720cf7637cb18e7bdabad
Sevcan Doğruyol <95580142+sevcand@users.noreply.github.com>
2022-05-29 19:31:36 +0300

    bandit_script_2

--- IR Unity/Assets/Scripts/Bandit/BanditMovement.cs
+++ IR Unity/Assets/Scripts/Bandit/BanditMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Experimental.GraphView;
@@ -5,13 +6,19 @@ using UnityEngine;
 
 public class BanditMovement : MonoBehaviour
 {
+    #region Public Variables
+    
     public Transform leeway;
     public LayerMask leeway_mask;
     public float leeway_length;
     public float attack_distance;
     public float moving_speed;
     public float timer;
+    
+    #endregion
 
+    #region Private Variables
+    
     private Animator anim;
     private GameObject target;
     private RaycastHit2D hit;
@@ -21,10 +28,42 @@ public class BanditMovement : MonoBehaviour
     private bool cooling;
     private float int_timer;
     
+    #endregion
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
+        int_timer = timer;
+        anim = GetComponent<Animator>();
         
     }
+
+    void Update()
+    {
+        if (in_range)
+        {
+            hit = Physics2D.Raycast(leeway.position, Vector2.left, leeway_length, leeway_mask);
+            RayCastDebugger();
+            
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D trig)
+    {
+        if (trig.gameObject.tag == "Player")
+        {
+            target = trig.gameObject;
+            in_range = true;
+            
+        }
+        
+    }
+
+    void RayCastDebugger()
+    {
+        if (distance > attack_distance) ;
+        Debug.DrawRay(leeway.position, Vector2.left * leeway_length, Color.red);
+        
+    }
+    
 }
