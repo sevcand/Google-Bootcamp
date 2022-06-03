9213ce4d8003d76926fe30d72be99a0ccbf15d50
Alperen <104386862+Apkods@users.noreply.github.com>
2022-06-04 00:19:29 +0300

    Düzenlemeler

--- IR Unity/Assets/Sprites/Animals/BirdController.cs
+++ IR Unity/Assets/Sprites/Animals/BirdController.cs
@@ -14,8 +14,14 @@ public class BirdController : MonoBehaviour
 
     int kacinciPozisyon;
 
+    Animator anim;
+
+    Vector2 kusYonu;
+
     private void Awake()
     {
+        anim = GetComponent<Animator>();
+
         foreach (Transform pos in pozisyonlar)
         {
             pos.parent = null;
@@ -34,10 +40,28 @@ public class BirdController : MonoBehaviour
         if (beklemeSayac > 0)
         {
             beklemeSayac -= Time.deltaTime;
+            anim.SetBool("ucsunMu", false);
         }
         else
         {
+
+            kusYonu = new Vector2(pozisyonlar[kacinciPozisyon].position.x - transform.position.x, pozisyonlar[kacinciPozisyon].position.y - transform.position.y);
+
+
+            float angle = Mathf.Atan2(kusYonu.y, kusYonu.x)*Mathf.Rad2Deg;
+
+            if (transform.position.x > pozisyonlar[kacinciPozisyon].position.x)
+            {
+                transform.localScale = new Vector3(1, -1, 1);
+            }else
+            {
+                transform.localScale = Vector3.one;
+            }
+            transform.rotation = Quaternion.Euler(0, 0, angle);
+
             transform.position = Vector3.MoveTowards(transform.position, pozisyonlar[kacinciPozisyon].position, birdSpeed * Time.deltaTime);
+
+            anim.SetBool("ucsunMu", true);
 
             if(Vector3.Distance(transform.position, pozisyonlar[kacinciPozisyon].position) < 0.1f)
             {
