4311ab89acbcfe75ef5471ca7788359ca7824db5
Alperen <104386862+Apkods@users.noreply.github.com>
2022-06-04 11:21:55 +0300

    Deneme

--- IR Unity/Assets/Bandits - Pixel Art/Sprites/Enemy3/BanditCont.cs
+++ IR Unity/Assets/Bandits - Pixel Art/Sprites/Enemy3/BanditCont.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BanditController : MonoBehaviour
+public class BanditCont : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
